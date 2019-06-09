8a2d0d46bdf2d88395b9c8ed5fa09cf8f5df0ad1
Kevin O'Brien <kpobrien@buffalo.edu>
2019-06-09 21:04:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -174,7 +174,7 @@ end
 def player_numbers(team)
   array2 = []
   game_hash.each do |location, team_data|
-      if team_data[:team_name] = team
+      if team_data[:team_name] == team
       team_data.each do |attribute, values|
           if attribute == :players
             values.each do |person|
