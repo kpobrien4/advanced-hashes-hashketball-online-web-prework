699e15e9ecf5912ba73474415e6b092c5124f9f6
Kevin O'Brien <kpobrien@buffalo.edu>
2019-06-09 20:09:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -200,11 +200,11 @@ def most_points_scored
 most_points = 0
  game_hash.each do |location, team_data|
    team_data[:players].each do |player|
+     binding.pry
      points = player[:points]
      if points > most_points
        most_points = points
        mvp = player[:player_name]
-       binding.pry
      end
    end
  end
