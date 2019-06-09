5a138c99d07c84b7b764c454cf5da228033cf1a4
Kevin O'Brien <kpobrien@buffalo.edu>
2019-06-09 18:12:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -118,10 +118,21 @@ def game_hash
 end
 
 
-def num_points_scored(name)
-  if game_hash[:home][:players][name]
-    game_hash[:home][:players][name][:points]
-  else
-    game_hash[:away][:players][name][:points]
-  end
+def num_points_scored(player_name)
+array2 = []
+game_hash.each do |location, team_data|
+    team_data.each do |attribute, values|
+        if attribute == :players
+          values.each do |person, data|
+            data.each do |i, j|
+              if person == player_name && i == :points
+                array2.push(j)
+              end
+
+            end
+          end
+        end
+    end
+end
+return array2[0]
 end
